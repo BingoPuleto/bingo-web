08907e81a9131af8dfd8e6f18b9b66865533fc85
kv-aliaga <kavhsao@gmail.com>
2026-07-23 00:22:22 -0300

    adiciona url da api em produção

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = Object.freeze({
-  API_BASE_URL: 'http://localhost:8080/api/v1',
-  WS_URL: 'http://localhost:8080/ws',
+  API_BASE_URL: 'https://bingo-api-0pex.onrender.com/api/v1',
+  WS_URL: 'https://bingo-api-0pex.onrender.com/ws',
   ROOM_CODE_LENGTH: 6,
   NICKNAME_MAX_LENGTH: 30,
 });
